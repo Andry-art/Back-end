e875868c36162b96975e53d91851c2f3be1cba03
Andrei Sasonka <andrei.sasonka@itechart-group.com>
2023-02-06 22:55:53 +0700

    add feald for steps

--- src/models/User.js
+++ src/models/User.js
@@ -3,6 +3,18 @@ import { Schema, model } from 'mongoose';
 const UserSchema = new Schema({
   email: { type: String, unique: true, required: true },
   password: { type: String, unique: true, required: true },
+  balance: { type: Number, required: true, default: 0 },
+  dailySteps: { type: Number, required: true, default: 0 },
+  dailyActiveType: { type: Number, required: true, default: 0 },
+  dailyDestenation: { type: Number, required: true, default: 0 },
+  dailyBalance: { type: Number, required: true, default: 0 },
+  stepsHistory: [
+    {
+      date: Date,
+      steps: Number,
+      tokens: Number,
+    },
+  ],
 });
 
 export default model('User', UserSchema);
