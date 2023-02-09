79b85f35b9ba3764e74b4f1468420bcf059e6a17
Andrei Sasonka <andrei.sasonka@itechart-group.com>
2023-02-10 02:43:35 +0700

    remove socket

--- index.js
+++ index.js
@@ -7,30 +7,30 @@ import cors from 'cors';
 import cookieParser from 'cookie-parser';
 import dotenv from 'dotenv';
 import errorMidleware from './src/middleWare/error-middleware.js';
-import { createServer } from 'http';
-import { Server } from 'socket.io';
-import HistoryOfSteps from './src/models/HistoryOfSteps.js';
+// import { createServer } from 'http';
+// import { Server } from 'socket.io';
+// import HistoryOfSteps from './src/models/HistoryOfSteps.js';
 
 dotenv.config();
 const DB_URL = process.env.DB_URL;
 const PORT = 4000;
 const app = express();
-const httpServer = createServer(app);
-const io = new Server(httpServer);
-io.on('connection', (socket) => {
-  socket.on('message', async (data) => {
-    const query = { userId: data.userId, date: data.date };
-    const update = { steps: data.steps, tokens: data.tokens };
-    const options = { upsert: true, new: true, setDefaultsOnInsert: true };
-    await HistoryOfSteps.findOneAndUpdate(query, update, options, (error, message) => {
-      if (error) {
-        console.error(error);
-      } else {
-        console.log('message');
-      }
-    });
-  });
-});
+// const httpServer = createServer(app);
+// const io = new Server(httpServer);
+// io.on('connection', (socket) => {
+//   socket.on('message', async (data) => {
+//     const query = { userId: data.userId, date: data.date };
+//     const update = { steps: data.steps, tokens: data.tokens };
+//     const options = { upsert: true, new: true, setDefaultsOnInsert: true };
+//     await HistoryOfSteps.findOneAndUpdate(query, update, options, (error, message) => {
+//       if (error) {
+//         console.error(error);
+//       } else {
+//         console.log('message');
+//       }
+//     });
+//   });
+// });
 
 app.use(
   helmet.contentSecurityPolicy({
@@ -52,8 +52,8 @@ const startApp = async () => {
   try {
     mongoose.set('strictQuery', true);
     await mongoose.connect(DB_URL);
-    httpServer.listen(PORT, () => console.log('SERVER STARTED ON PORT ' + PORT));
-    // app.listen(PORT, () => console.log('SERVER STARTED ON PORT ' + PORT));
+    // httpServer.listen(PORT, () => console.log('SERVER STARTED ON PORT ' + PORT));
+    app.listen(PORT, () => console.log('SERVER STARTED ON PORT ' + PORT));
   } catch (e) {
     console.log(e);
   }
