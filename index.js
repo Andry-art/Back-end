236da3fec5879bf0bfcf7d3014702d6f10a92e13
Andrei Sasonka <andrei.sasonka@itechart-group.com>
2023-02-10 02:36:18 +0700

    fix

--- index.js
+++ index.js
@@ -10,34 +10,26 @@ import errorMidleware from './src/middleWare/error-middleware.js';
 import { createServer } from 'http';
 import { Server } from 'socket.io';
 import HistoryOfSteps from './src/models/HistoryOfSteps.js';
-import { MongoClient } from 'mongodb';
-import { createAdapter } from '@socket.io/mongo-adapter'
 
 dotenv.config();
 const DB_URL = process.env.DB_URL;
 const PORT = 4000;
-const COLLECTION = "socket.io-adapter-events";
 const app = express();
 const httpServer = createServer(app);
 const io = new Server(httpServer);
 io.on('connection', (socket) => {
   socket.on('message', async (data) => {
-    console.log(data, 'srvsrvsrvsrv')
-    // const query = { userId: data.userId, date: data.date };
-    // const update = { steps: data.steps, tokens: data.tokens };
-    // const options = { upsert: true, new: true, setDefaultsOnInsert: true };
-    // await HistoryOfSteps.findOneAndUpdate(query, update, options, (error, message) => {
-    //   if (error) {
-    //     console.error(error);
-    //   } else {
-    //     console.log('message');
-    //   }
-    // });
+    const query = { userId: data.userId, date: data.date };
+    const update = { steps: data.steps, tokens: data.tokens };
+    const options = { upsert: true, new: true, setDefaultsOnInsert: true };
+    await HistoryOfSteps.findOneAndUpdate(query, update, options, (error, message) => {
+      if (error) {
+        console.error(error);
+      } else {
+        console.log('message');
+      }
+    });
   });
-});
-
-const mongoClient = new MongoClient("mongodb://localhost:27017/?replicaSet=rs0", {
-  useUnifiedTopology: true,
 });
 
 app.use(
@@ -57,22 +49,14 @@ app.use('/data', userInfoRoute);
 app.use(errorMidleware);
 
 const startApp = async () => {
-  await mongoClient.connect();
   try {
-    await mongoClient.db(DB_URL).createCollection(COLLECTION, {
-      capped: true,
-      size: 1e6
-    });
     mongoose.set('strictQuery', true);
     await mongoose.connect(DB_URL);
-    app.listen(PORT, () => console.log('SERVER STARTED ON PORT ' + PORT));
+    httpServer.listen(PORT, () => console.log('SERVER STARTED ON PORT ' + PORT));
+    // app.listen(PORT, () => console.log('SERVER STARTED ON PORT ' + PORT));
   } catch (e) {
     console.log(e);
   }
-  const mongoCollection = mongoClient.db(DB_URL).collection(COLLECTION);
-
-  io.adapter(createAdapter(mongoCollection));
-  io.listen(3000);
 };
 
 startApp();
