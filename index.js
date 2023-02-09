68cc02aae49478e7f0b01796032632739c435704
Andrei Sasonka <andrei.sasonka@itechart-group.com>
2023-02-10 03:26:47 +0700

    last try

--- index.js
+++ index.js
@@ -6,11 +6,12 @@ import userInfoRoute from './src/routers/userInfoRoute.js';
 import cors from 'cors';
 import cookieParser from 'cookie-parser';
 import dotenv from 'dotenv';
-import errorMidleware from './src/middleWare/error-middleware.js';
+import errorMiddleware from './src/middleware/error-middleware.js';
 import { createServer } from 'http';
 import { Server } from 'socket.io';
+import mongodb from 'mongodb';
+import MongoAdapter from '@socket.io/mongo-adapter';
 import HistoryOfSteps from './src/models/HistoryOfSteps.js';
-import SocketIoMongoDbAdapter from '@socket.io/mongo-adapter';
 
 dotenv.config();
 const DB_URL = process.env.DB_URL;
@@ -18,21 +19,25 @@ const PORT = 4000;
 const app = express();
 const httpServer = createServer(app);
 const io = new Server(httpServer);
-const adapter = new SocketIoMongoDbAdapter(DB_URL);
+const mongo = mongodb.MongoClient;
+mongo.connect(DB_URL, { useUnifiedTopology: true }, (err, client) => {
+  if (err) throw err;
+  const db = client.db('db');
+  const adapter = new MongoAdapter(db);
+  io.adapter(adapter);
 
-io.adapter(adapter);
-
-io.on('connection', (socket) => {
-  socket.on('message', async (data) => {
-    const query = { userId: data.userId, date: data.date };
-    const update = { steps: data.steps, tokens: data.tokens };
-    const options = { upsert: true, new: true, setDefaultsOnInsert: true };
-    await HistoryOfSteps.findOneAndUpdate(query, update, options, (error, message) => {
-      if (error) {
-        console.error(error);
-      } else {
-        console.log('message');
-      }
+  io.on('connection', (socket) => {
+    socket.on('message', async (data) => {
+      const query = { userId: data.userId, date: data.date };
+      const update = { steps: data.steps, tokens: data.tokens };
+      const options = { upsert: true, new: true, setDefaultsOnInsert: true };
+      await HistoryOfSteps.findOneAndUpdate(query, update, options, (error, message) => {
+        if (error) {
+          console.error(error);
+        } else {
+          console.log('message');
+        }
+      });
     });
   });
 });
@@ -51,14 +56,12 @@ app.use(cors());
 
 app.use('/auth', authRouter);
 app.use('/data', userInfoRoute);
-app.use(errorMidleware);
+app.use(errorMiddleware);
 
 const startApp = async () => {
   try {
-    mongoose.set('strictQuery', true);
-    await mongoose.connect(DB_URL);
-    // httpServer.listen(PORT, () => console.log('SERVER STARTED ON PORT ' + PORT));
-    app.listen(PORT, () => console.log('SERVER STARTED ON PORT ' + PORT));
+    await mongoose.connect(DB_URL, { useUnifiedTopology: true });
+    httpServer.listen(PORT, () => console.log('Server started on port ' + PORT));
   } catch (e) {
     console.log(e);
   }
