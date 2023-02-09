f52aff91496d50bf771528f997e0e7d6817ad644
Andrei Sasonka <andrei.sasonka@itechart-group.com>
2023-02-10 03:31:58 +0700

    back

--- index.js
+++ index.js
@@ -6,41 +6,12 @@ import userInfoRoute from './src/routers/userInfoRoute.js';
 import cors from 'cors';
 import cookieParser from 'cookie-parser';
 import dotenv from 'dotenv';
-import errorMidleware from './src/middleWare/error-middleware.js'
-import { createServer } from 'http';
-import { Server } from 'socket.io';
-import mongodb from 'mongodb';
-import MongoAdapter from '@socket.io/mongo-adapter';
-import HistoryOfSteps from './src/models/HistoryOfSteps.js';
+import errorMidleware from './src/middleWare/error-middleware.js';
 
 dotenv.config();
 const DB_URL = process.env.DB_URL;
 const PORT = 4000;
 const app = express();
-const httpServer = createServer(app);
-const io = new Server(httpServer);
-const mongo = mongodb.MongoClient;
-mongo.connect(DB_URL, { useUnifiedTopology: true }, (err, client) => {
-  if (err) throw err;
-  const db = client.db('db');
-  const adapter = new MongoAdapter(db);
-  io.adapter(adapter);
-
-  io.on('connection', (socket) => {
-    socket.on('message', async (data) => {
-      const query = { userId: data.userId, date: data.date };
-      const update = { steps: data.steps, tokens: data.tokens };
-      const options = { upsert: true, new: true, setDefaultsOnInsert: true };
-      await HistoryOfSteps.findOneAndUpdate(query, update, options, (error, message) => {
-        if (error) {
-          console.error(error);
-        } else {
-          console.log('message');
-        }
-      });
-    });
-  });
-});
 
 app.use(
   helmet.contentSecurityPolicy({
@@ -60,8 +31,9 @@ app.use(errorMidleware);
 
 const startApp = async () => {
   try {
-    await mongoose.connect(DB_URL, { useUnifiedTopology: true });
-    httpServer.listen(PORT, () => console.log('Server started on port ' + PORT));
+    mongoose.set('strictQuery', true);
+    await mongoose.connect(DB_URL);
+    app.listen(PORT, () => console.log('SERVER STARTED ON PORT ' + PORT));
   } catch (e) {
     console.log(e);
   }
